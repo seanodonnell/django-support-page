02483f933f82159bdf9ebcacb5b3c69c62eddd78
Sean O'Donnell <sean@odonnell.nu>
2011-02-11 03:24:05 +0000

    named index url

--- django-support-page/urls.py
+++ django-support-page/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    (r'^$', direct_to_template, {"template":"django-support-page/support.html"}),
+    url(r'^$', direct_to_template, {"template":"django-support-page/support.html"}, name="support_index"),
 )
