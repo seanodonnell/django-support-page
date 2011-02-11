cd6f03bea78b51e4481a54707d77981db267815f
Sean O'Donnell <sean@odonnell.nu>
2011-02-11 02:57:29 +0000

    hooking up root url

--- example/urls.py
+++ example/urls.py
@@ -1,16 +1,10 @@
 from django.conf.urls.defaults import *
-
+from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^example/', include('example.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^$', direct_to_template, {"template":"index.html"}),
+    (r'^support/', include("django-support-page.urls")),
 )
