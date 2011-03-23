3c1dda8a6ea7f34fd94804efdb5360e484038857
Sean O'Donnell <sean@odonnell.nu>
2011-03-23 20:21:36 +0000

    added staticfiles serving to example apps urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,3 +9,8 @@ urlpatterns = patterns('',
     (r'^$', direct_to_template, {"template":"index.html"}),
     (r'^support/', include("django_support_page.urls")),
 )
+
+if settings.DEBUG:
+        urlpatterns += patterns('django.contrib.staticfiles.views',
+                url(r'^static/(?P<path>.*)$', 'serve'),
+                    )
