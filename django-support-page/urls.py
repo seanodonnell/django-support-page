5dae0fb8b83ca371349d9a1999e69a7bcba9d2a8
Sean O'Donnell <sean@odonnell.nu>
2011-03-23 18:54:06 +0000

    name change

--- django-support-page/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
-
-urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {"template":"django-support-page/support.html"}, name="support_index"),
-)
